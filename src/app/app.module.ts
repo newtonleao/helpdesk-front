f1175c4fbdb386369b798c272f33a82a2cc0823a
Newton Magalhães <newtonleao@hotmail.com>
2021-09-13 14:17:56 -0300

    componente NAV criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,9 +28,14 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 
+// componentes do projeto
+import { NavComponent } from './components/nav/nav.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
