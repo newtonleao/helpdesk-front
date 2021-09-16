a2dee1c2b4ec9b97a5a5406a81439873f78ce717
Newton Magalhães <newtonleao@hotmail.com>
2021-09-16 12:48:22 -0300

    HTML e CSS para tecnico-list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'models/tecnicos';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+    nome: 'Newton',
+    cpf: '513.924.885-91',
+    email: "newtonleao@hotmail.com",
+    senha: '123',
+    perfis: ['0'],
+    dataCriacao: '10,10,2021'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
